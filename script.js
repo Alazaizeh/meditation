dbc0ef9c650f1b9dc323ebab37938a813e383a4a
Omx302 <>
2021-04-14 12:33:34 +0000

    js update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ alert("Welcome " +Name );
 if (confirm("Would you like to visit our Meditation guide ?")) {
   window.location.href ="howto.html";
 } else {
-  var x = prompt("Do you like 'dogs' or 'cats'?")
+  var x = prompt("OK !! Do you like 'dogs' or 'cats'?")
   if(x=="dogs"){
   var image = '<img src="https://i.ibb.co/tPn3qxM/82842a3426446a5db6b9d34446cf7c97-removebg-preview.png" alt= "dog"/>';
   document.write(image);
