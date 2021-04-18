1bb2fde7517a57a73b76845aeecee3b0e1018187
Omx302 <>
2021-04-18 11:14:07 +0000

    lab update

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 var Name = prompt('Please enter your name');
-alert("Welcome " +Name );
+alert("Welcome " + Name);
+
+function ChooseOne(A,B) {
+  var ans = "";
+  while (ans != A && ans != B) {
+    ans = prompt("Do you like '"+A+"' or '"+B+"'?")
+  }
+  return ans;
+}
+
+function DoYouLike(item) {
+ var x = -1;
+  while (x < 0 || x > 10) {
+    x = prompt("How much do you like " + item + " 1 - 10?")
+  }
+  return x;
+}
+
+function DrawImg(alt, link, num) {
+  image = '<img src="' + link + '" alt= "' + alt + '"/>';
+  for (var i = 0; i < num; i++) {
+    document.write(image);
+  }
+  window.scrollTo(0, document.body.scrollHeight);
+
+}
+
 
 if (confirm("Would you like to visit our Meditation guide ?")) {
-  window.location.href ="howto.html";
+  window.location.href = "howto.html";
 } else {
 
-  var animal = prompt("OK !! Do you like 'dogs' or 'cats'?")
-  
-  while( animal!="cats" && animal!="dogs") {
-  animal = prompt("Do you like 'dogs' or 'cats'?")
-  }
+  var animal = ChooseOne("cats","dogs");
 
-  var x = prompt("How much do you like "+animal+" 1 - 10?")
-  while( x<0 || x>10) {
-  x = prompt("How much do you like "+animal+" 1 - 10?")
-  }
+  var score = DoYouLike(animal);
 
 
+  if (animal == "dogs") {
+    var link = "https://i.ibb.co/tPn3qxM/82842a3426446a5db6b9d34446cf7c97-removebg-preview.png";
 
-  if(animal=="dogs"){
-  var image = '<img src="https://i.ibb.co/tPn3qxM/82842a3426446a5db6b9d34446cf7c97-removebg-preview.png" alt= "dog"/>';
-  for(var i=0;i<x;i++)
-  {
-    document.write(image);
+    DrawImg(animal, link, score);
   }
-  window.scrollTo(0,document.body.scrollHeight);
-  }
-  else if(animal=="cats"){
-  var image = '<img src="https://i.ibb.co/bQQKXBM/meditating-cat-illustrated-greetings-card-matt-witt-illustration-removebg-preview.png" alt= "cat"/>';
-  for(var i=0;i<x;i++)
-  {
-    document.write(image);
-  }
-  window.scrollTo(0,document.body.scrollHeight);
+  else if (animal == "cats") {
+    var link = "https://i.ibb.co/bQQKXBM/meditating-cat-illustrated-greetings-card-matt-witt-illustration-removebg-preview.png";
+
+    DrawImg(animal, link, score);
 
   }
 }
